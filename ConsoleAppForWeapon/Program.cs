9d97c73505ffdb889718746abc138a129b1f9d39
Abbas0vv <ilhamabbasov46@gmail.com>
2025-03-27 04:55:54 +0400

    Task Fully Completed

---
+++ ConsoleAppForWeapon/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleAppForWeapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Enter the initial bullet count: ");
+            int bullets = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the maximum bullet count: ");
+            int maxBullets = int.Parse(Console.ReadLine());
+
+            Console.Write("Should the weapon be automatic? (true/false): ");
+            bool isAuto = bool.Parse(Console.ReadLine());
+
+            Weapon myWeapon = new Weapon(bullets, maxBullets, isAuto);
+
+            while (true)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün (hazirki moda uygun gulle atacaq)");
+                Console.WriteLine("2 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("3 - Reload metodu üçün");
+                Console.WriteLine("4 - ChangeFireMode metodu üçün");
+                Console.WriteLine("5 - Proqramdan dayandırmaq üçün");
+                Console.WriteLine();
+
+                int userChoise = int.Parse(Console.ReadLine());
+
+                switch (userChoise)
+                {
+                    case 0:
+                        Console.WriteLine($"Sizin silahınızın güllə sayı: {myWeapon._bullets}. Maximum güllə sayı : {myWeapon._maxBullets}. Silahınız {(myWeapon._isAuto ? "Avtomatik" : "Tək-tək")} atış modundadır");
+                        break;
+
+                    case 1:
+                        myWeapon.Shoot();
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"Doldurmaq üçün lazım olan güllə sayı: {myWeapon.GetRemainBulletCount()}");
+                        break;
+
+                    case 3:
+                        myWeapon.Reload();
+                        Console.WriteLine("Silah reload edildi!");
+                        break;
+
+                    case 4:
+                        myWeapon.ChangeFireMode();
+                        Console.WriteLine($"{(myWeapon._isAuto ? "Avtomatik" : "Tək-tək")} atış moduna keçdi.");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Proqram dayandırıldı.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Yanlış seçim, yenidən daxil et.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppForWeapon/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForWeapon
+{
+    class Weapon
+    {
+        public Weapon(int bullets, int maxBullets, bool isAuto)
+        {
+            _bullets = bullets;
+            _maxBullets = maxBullets;
+            _isAuto = isAuto;
+        }
+
+        public int _bullets { get; set; }
+        public int _maxBullets { get; set; }
+        public bool _isAuto { get; set; }
+
+
+        public void Shoot()
+        {
+            if (_bullets > 0)
+            {
+                if (_isAuto)
+                {
+                    while (_bullets > 0)
+                    {
+                        Console.Write("Pew ");
+                        _bullets--;
+                    }
+                    Console.WriteLine("Auto Mode");
+                }
+                else
+                {
+                    Console.WriteLine("Pew");
+                    Console.WriteLine("Single Mode");
+                    _bullets--;
+                }
+
+            }
+        }
+            
+
+        public int GetRemainBulletCount()
+        {
+            int needForBullets = _maxBullets - _bullets;
+            return needForBullets;
+        }
+
+        public void Reload()
+        {
+            if ( _bullets < _maxBullets )
+            {
+                _bullets = _maxBullets;
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            if (_isAuto)
+            {
+                _isAuto = false;
+            }
+            else
+            {
+                _isAuto = true;
+            }
+        }
+    }
+}
